f258a2efe2dfa24e6fa65516784c5d83f73d0b26
pledgedavis <pledgedavis14@gmail.com>
2019-12-13 08:24:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
     
  def speak_to_grandma(string)
-#   if string == 'I LOVE YOU GRANDMA'
-#     "I LOVE YOU TOO PUMPKIN"
-#   elsif string == string.upcase
-#   "NO, NOT SINCE 1938"
-#   elsif string != string.upcase 
-#   "HUH?! SPEAK UP, SONNY!"
-# end
+   if string == 'I LOVE YOU GRANDMA'
+     "I LOVE YOU TOO PUMPKIN"
+  elsif string == string.upcase
+   "NO, NOT SINCE 1938"
+   elsif string != string.upcase 
+   "HUH?! SPEAK UP, SONNY!"
+ end
